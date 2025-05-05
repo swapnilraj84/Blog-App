3038014670d440651b160804ad68e658b826834d
Swapnil Raj <swapnilraj85444@gmail.com>
2025-05-05 23:12:06 +0530

    Final Changes Done

--- Blogging/urls.py
+++ Blogging/urls.py
@@ -3,9 +3,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
      path('admin/', admin.site.urls),
      path('blog/', include('blog.urls')), # Include the blog app URLs
      path('accounts/', include('django.contrib.auth.urls')), # Include the default auth URLs
+     path('', RedirectView.as_view(url='/blog/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
